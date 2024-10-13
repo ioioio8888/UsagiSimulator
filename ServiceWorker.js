6c12c0fd10ae47285f205c17a13ff0d578f8280a
Louis Lui <louislui95111@gmail.com>
2024-10-14 00:32:57 +0800

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "ioioio8888-UsagiSimulator-1.0";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/Build.framework.js.unityweb",
+    "Build/Build.data.unityweb",
+    "Build/Build.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
